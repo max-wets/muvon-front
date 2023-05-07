845f923737a050b0db96f17df5a6cb2e6f35bbbe
Maxime Wets <wetsmaxime@gmail.com>
2023-05-07 15:07:45 +0200

    💄 header: UI done

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  faGlobe,
+  faCircleUser,
+  faBars,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  faGlobe = faGlobe;
+  faCircleUser = faCircleUser;
+  faBars = faBars;
 }
